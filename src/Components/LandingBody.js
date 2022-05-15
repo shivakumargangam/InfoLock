fb57029aa1cc27db949f526f3b5b14d8fe06035b
DESKTOP-ANQ9B4T <gangamshivakumar01.com>
2022-05-15 11:45:08 +0530

    f

---
+++ src/Components/LandingBody.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const LandingBody = () => {
+    return ( 
+        <div>
+            <div className=''>
+
+
+            </div>
+
+
+            <div>
+
+
+
+            </div>
+
+
+
+        </div>
+
+
+     );
+}
+ 
+export default LandingBody;
