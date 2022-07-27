d64b69ffa8fdd25dd8808f86f41c806977916792
DESKTOP-ANQ9B4T <gangamshivakumar01.com>
2022-07-27 23:00:51 +0530

    DecryptionPage

--- src/Components/PopupBox.js
+++ src/Components/PopupBox.js
@@ -3,7 +3,16 @@ import '../Stylesheet/PopupBox.css';
 const PopupBox = () => {
     return ( 
         <div className='PopupmainDiv'>
-            
+            <div className='PopBox'>
+                <div className='PopupTitle'>
+                    <span>Enter the Decryption Key</span>
+                </div>
+                <input className='PopupInput'></input>
+                <div className='SubmitButton'>
+                    <span>submit</span>
+                </div>
+
+            </div>
         </div>
      );
 }
